caa3f6c2a75687188e3340131da29c2f4f6f9f42
IsmaelCajar <126801854+IsmaelCajar@users.noreply.github.com>
2026-04-14 22:04:03 -0500

    Update steam-bot.py

--- steam-bot.py
+++ steam-bot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import aiohttp
 
-TOKEN = "TU_TOKEN_AQUI"
+TOKEN = "COLOCAR TOKEN AQUI"
 
 intents = discord.Intents.default()
 intents.message_content = True
